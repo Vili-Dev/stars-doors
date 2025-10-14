297e6115c1654e19165c229c6692238c8212dc58
S-HASSAN-ABDALLAH <shadah.hassan.abdallah@gmail.com>
2025-10-14 22:18:50 +0200

    feat(admin): création page statistiques
    Ajout de admin/statistiques.php :
    - Initialisation session et sécurité
    - Include header
    - Navbar complète avec 3 liens
    - Protection requireLogin et requireAdmin

--- admin/statistiques.php
+++ admin/statistiques.php
@@ -14,7 +14,20 @@ requireAdmin();
 $title = 'Statistiques - Administration';
 
 include '../includes/header.php';
-include '../includes/navbar.php'
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="../index.php">Stars Doors</a>
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link" href="index.php">Admin</a>
+                <a class="nav-link" href="../dashboard.php">Retour au site</a>
+                <a class="nav-link" href="../logout.php">Déconnexion</a>
+            </div>
+    </div>
+</nav>
+
+
 
 
 
